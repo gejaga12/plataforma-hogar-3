4043d0f091299529a79ccd4d88dd57c9e6ef14ad
Lucas <lukassmierez@gmail.com>
2025-07-21 20:27:52 -0300

    modifico 404 not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,25 +1,26 @@
+import { Home } from "lucide-react";
 import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-white px-4 text-center">
+    <div className="min-h-screen relative flex flex-col justify-center items-center bg-white px-4 text-center overflow-hidden">
+      {/* Ícono Home como fondo decorativo */}
+      <Home
+        className="absolute z-0 text-orange-500 left-1/3 top-1/2 transform -translate-x-[30%] -translate-y-1/2 opacity-40"
+        size={300}
+      />
       <div className="max-w-md">
-        {/* <img
-          src="/images/404-illustration.svg"
-          alt="Not Found Illustration"
-          className="w-64 mx-auto mb-6"
-        /> */}
         <h1 className="text-[8rem] font-extrabold text-orange-600 leading-none">
           404
         </h1>
-        <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mb-4">
-          ¡Ups! Página no encontrada
+        <h2 className="text-3xl md:text-4xl font-bold text-black mb-4 bg-white/70 p-1 rounded-lg">
+          Página no encontrada
         </h2>
-        <p className="text-gray-600 mb-6">
+        <p className="text-black mb-6">
           Lo sentimos, no pudimos encontrar la página que estás buscando.
         </p>
         <a
-          href="/#"
+          href="/dashboard"
           className="inline-block bg-orange-600 hover:bg-orange-700 text-white font-semibold py-3 px-6 rounded-lg transition duration-300"
         >
           Volver al inicio
